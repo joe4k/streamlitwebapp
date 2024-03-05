662467607fdad51e7b729ae1b8a7a2a30c33bf00
kozhaya <kozhaya@kozhayas-mbp.dfw.ibm.com>
2024-03-05 10:00:05 -0600

    updates

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ import random
 
 # Title
 st.title("RPA Demo")
-st.session_state["user_input"] = st.text_input(label="Account number")
+
+# Session State
+if "user_input" not in st.session_state:
+    st.session_state["user_input"] = ""
+
+if "outresult" not in st.session_state:
+    st.session_state['outresult'] = ''
+
+placeholder = st.empty()
+
+#st.session_state["user_input"] = placeholder.text_input(label="Account number")
 #st.session_state["outresult"]=""
 
 ###policy_check=""
@@ -17,6 +27,7 @@ def getQualityCheck(account_number):
         policy_check = account_number + ","
     
     st.session_state["outresult"]=policy_check
+    st.session_state["user_input"]=policy_check
 
 # Header
 #st.header("Account Number") 
@@ -30,7 +41,18 @@ def getQualityCheck(account_number):
 #    policy_check = account_number + ",3011"
 #else:
 #    policy_check = account_number + ","
-st.button("Go", on_click=getQualityCheck, args=(st.session_state["user_input"],))
+#st.button("Go", on_click=getQualityCheck, args=(st.session_state["user_input"],))
+if st.button("Go"):
+    r = random.uniform(0, 1)
+    #print("r: ", r)
+    if r < 0.5:
+        policy_check = st.session_state["user_input"] + ",5044"
+    else:
+        policy_check = st.session_state["user_input"]  + ","
+
+    st.session_state["user_input"] = policy_check
+
+user_input = placeholder.text_input(label="Account number",key="user_input")
 
 #st.button("Go", on_click=getQualityCheck())
-st.write("The account number is: ", st.session_state["outresult"])
+#st.write("The account number is: ", st.session_state["outresult"])
